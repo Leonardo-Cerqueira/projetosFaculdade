8012c1773c73983cfea1a880bdf55f4d045385ed
Leonardo Cerqueira <aryflonder18@gmail.com>
2023-08-25 00:21:27 -0300

    Arquivo V2

--- Clientes.py
+++ Clientes.py
@@ -1,9 +1,19 @@
+
+
+
+
+
+
 import pandas as pd
 
-class visualisarCad():
+# Pl = pd.read_excel("C:\Meus Projetos\projetosFaculdade\Planilhateste.xlsx")
+df = pd.read_excel("C:\\TESTE GIT 2\\projetosFaculdade\\Planilhateste.xlsx")
+df.head(42)
 
-    Pl = pd.read_excel("C:\Meus Projetos\projetosFaculdade\Planilhateste.xlsx")
 
-    df_teste = Pl
 
-    print(df_teste)
+class VisualisarCad:
+
+    @staticmethod
+    def clientes():
+        return print(df.head(41))
--- TesteGravacao.py
+++ TesteGravacao.py
@@ -1,34 +1,12 @@
-import pandas as pd
-from Clientes import visualisarCad
+from Clientes import VisualisarCad
+
+instancia = VisualisarCad
+
 
 class Main:
-
     print("---------------------------------------INÍCIO DO PROGRAMA---------------------------------------")
     print("Deseja visualizar os cadastros da planilha?(Sim/Não)")
-    escolha = input()
+    escolha = str(input())
 
-   ## if ((escolha == 'SIM') or (escolha == 'sim')):
-  ##         visualisarCad()
-        
-            
-"""print("Quantos telefones você deseja cadastrar?")
-QTD = int(input())
-
-cont = 0
-numero = [QTD]
-
-for i in range (QTD):
-    cont = cont + 1
-    print(f"Digite o {cont}º de telefone")
-    numero = input()
-    with open("numeros.txt", "a") as gravar:
-        gravar.write("Registro ")
-        gravar.write(str(cont))
-        gravar.write(" Número do telefone")
-        gravar.write(" " + numero + '\n')
-
-print('\n' + "fim prog")
-
-
-##with open("numeros.txt", "r") as ler:
-# ##   print(numero)"""
+    if (escolha == 'SIM') or (escolha == 'sim'):
+        instancia.clientes()
