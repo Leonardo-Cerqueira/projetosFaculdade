a7e972c27be1703296ae016e3670a9e552bc3850
Leonardo Cerqueira <aryflonder18@gmail.com>
2023-08-26 02:34:39 -0300

    Programa V4

---
+++ AltPlan.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import openpyxl
+import os
+from openpyxl import load_workbook
+from openpyxl import workbook
+
+
+#df  = pd.read_excel("C:\\Meus Projetos\\projetosFaculdade\\Planilhateste.xlsx")
+df = pd.read_excel("C:\\TESTE GIT 2\\projetosFaculdade\\Planilhateste.xlsx")
+df.head(41)
+
+planilha = load_workbook(filename= 'Planilhateste.xlsx')
+plan = planilha.active
+
+class Escrever:
+
+    def alterar():
+        os.system("cls")
+        print("Escolha a coluna a ser alterada:")
+        coluna = int(input())
+        print("\n" + "Escolha a linha a ser alterada:")
+        linha  = int(input())
+        print("\n Escolha o novo valor a ser inserido:")
+        valor  = input()
+
+        plan.cell(row=linha,column=coluna,value=valor)
+        plan.template = True
+        planilha.save('Planilhateste.xlsx')
+        
---
+++ MenuPrincipal.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import openpyxl
+import os
+from openpyxl import load_workbook
+from openpyxl import workbook
+from  ViewPlan import VisualisarCad
+from  AltPlan  import Escrever
+
+instancia = VisualisarCad
+instacia2 = Escrever   
+
+
+
+class Main:
+
+    while True:
+      print("---------------------------------------INÍCIO DO PROGRAMA---------------------------------------")
+      print("|Escolha uma opção:                                                                            |")
+      print("------------------------------------------------------------------------------------------------")
+      print("|Ler cadastros da planilha(1)                 | Alterar cadastros da planilha(2)               |")
+      print("------------------------------------------------------------------------------------------------")
+      print("|                                     Finalizar programa(3)                                    |")
+      print("------------------------------------------------------------------------------------------------")
+      escolha = input()
+      if (escolha == '1'):
+        instancia.clientes()
+        escolha2 = (input("\n" + "Digite -sim- para voltar ao menu principal" + "\n")).upper()
+        if (escolha2 != "SIM"):
+           print("---------------------------------------FIM DO PROGRAMA------------------------------------------")
+           break  
+      if (escolha == '2'):
+        instacia2.alterar()
+        escolha2 = (input("Digite -sim- para voltar ao menu principal" + "\n")).upper()
+        if (escolha2 != "SIM"):
+           print("---------------------------------------FIM DO PROGRAMA------------------------------------------")
+           break  
+      if (escolha == '3'):
+           print("---------------------------------------FIM DO PROGRAMA------------------------------------------")
+           break
+  
