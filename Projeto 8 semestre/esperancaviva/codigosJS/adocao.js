e930649e65531fea56655c3c6acf2437c4feff93
leonardo <aryflonder18@gmail.com>
2024-11-08 22:04:10 -0300

    Subindo função adoção js

--- Projeto 8 semestre/esperancaviva/codigosJS/adocao.js
+++ Projeto 8 semestre/esperancaviva/codigosJS/adocao.js
@@ -1,33 +1,71 @@
-            const xhr = new XMLHttpRequest();
-            xhr.open("GET", "http://localhost:8080/pet/server");
-            xhr.responseType = "json";
-            xhr.send();
+const xhr = new XMLHttpRequest();
+xhr.open("GET", "http://localhost:8080/pet/server");
+xhr.responseType = "json";
+xhr.send();
 
-            xhr.onload = () => {
-              if (xhr.status == 200) {
-                const response = xhr.response;
+xhr.onload = () => {
+  if (xhr.status == 200) {
+    const response = xhr.response;
 
-                const animalsContainer = document.getElementById("animais");
-                animalsContainer.innerHTML = "";
+    const animalsContainer = document.getElementById("animais");
+    animalsContainer.innerHTML = "";
 
-                response.forEach(animals => {
-                  const { ID_ANIMAL, ESPECIE, NOME_RACA, NOME, GENERO, ESTADO} = animals;
+    response.forEach(animals => {
+      const { ID_ANIMAL, ESPECIE, NOME_RACA, NOME, GENERO, ESTADO } = animals;
 
-                  const animalsDiv = document.createElement("div");
-                  animalsDiv.classList.add("animals");
+      const animalsForm = document.createElement("form");
+      animalsForm.id = `Form ${ID_ANIMAL}`;
+      animalsForm.classList.add("animals");
 
-                  animalsDiv.innerHTML = `
-                      <p class="btn">Número do animal: ${ID_ANIMAL}</p>
-                      <h2>Espécie: ${ESPECIE}</h2>
-                      <h2>Nome da Raça: ${NOME_RACA}</h2>
-                      <h2>Nome do Animal: ${NOME}</h2>
-                      <h2>Gênero: ${GENERO}</h2>
-                      <h2>Estado em que foi encontrado: ${ESTADO}</h2>`;
+      animalsForm.innerHTML = `
+                      <h1>Registro animal: ${ID_ANIMAL}</h1>
+                      <h4>Espécie: ${ESPECIE} <br> 
+                      Nome da Raça: ${NOME_RACA} <br>
+                      Nome do Animal: ${NOME} <br> 
+                      Gênero: ${GENERO} <br> 
+                      Estado em que foi encontrado: ${ESTADO} <br> 
+                      <input type="submit" value="Adotar">
+                      <input id='${ID_ANIMAL}' type="number" value='${ID_ANIMAL}' hidden>
+                      </h4>`;
 
-                  animalsContainer.appendChild(animalsDiv);
+      animalsContainer.appendChild(animalsForm);
 
-                  });
-               } else {
-                console.log(`Error: ${xhr.status}`);
-              }
-            };
+      animalsForm.addEventListener('submit', evento => {
+
+        evento.preventDefault();
+
+        var adot = "1";
+        var id = document.getElementById(`${ID_ANIMAL}`).value;
+
+        const xhr = new XMLHttpRequest();
+
+        xhr.open("POST", "http://localhost:8080/pet/server");
+
+        xhr.setRequestHeader("Content-Type", "application/json");
+        var corp = JSON.stringify({
+          id: id,
+          adot: adot
+        });
+
+        console.log(corp);
+
+        xhr.send(corp);
+
+        xhr.onload = () => {
+          console.log(xhr.status);
+          xhr.abort();
+          console.log("Conexão fechada")
+          location.reload(true);
+
+          window.alert("Parabéns!!! Sua intenção de adoção foi registrada!!! Diriga-se ao endereço da ong para completar a adoção. Leve os documentos necessários para realizar seu cadastro (RG, Comprovante de residência etc)!");
+
+        };
+
+
+      });
+    });
+  } else {
+    console.log(`Error: ${xhr.status}`);
+  }
+
+};
